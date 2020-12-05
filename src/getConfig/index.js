d18e57a433b4da6c145064d4c36821ce290488b7
Tom Chesters <tom@tomaaato.xyz>
2020-12-05 21:33:34 +0000

    BugFix: getConfig

--- src/getConfig/index.js
+++ src/getConfig/index.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 
 const getConfig = async () => {
     const configJson = (await fs.readFile('./repos.json')).toString()
-    const config = JSON.parse(reposJson)
+    const config = JSON.parse(configJson)
     return config
 }
 
